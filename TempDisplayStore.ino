65299f2fabdc8fd217901da972aa817d91568e77
codeasf <code_asf@126.com>
2015-08-29 14:37:41 +0800

    第一版
    改用DS18B02 可以显示小数点

--- TempDisplayStore.ino
+++ TempDisplayStore.ino
@@ -4,6 +4,8 @@
 #include <Wire.h>
 #include <DS1307RTC.h>
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 
 #define ONE_WIRE_BUS 2
@@ -11,16 +13,17 @@
 
 LiquidCrystal_I2C lcd(0x20,16,2);  // set the LCD address to 0x20 for a 16 chars and 2 line display
 File myFile;
-DHT dht;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 tmElements_t tm;
 
 char buff[17];
 byte subSecond = 0;
-float temperature = 0.00, humidity = 0.00;
+float temperature = 0.00;
 unsigned long time = 0;
 
 void setup() {
-	dht.setup(ONE_WIRE_BUS);
+	sensors.begin();
 	Serial.begin(9600);
 
 	lcd.init();                      // initialize the lcd 
@@ -29,10 +32,12 @@ void setup() {
 
 void loop() {
 	RTC.read(tm);
-	if (subSecond != tm.Second){
+	if (subSecond != second() ){
 		time = millis();
-
-		subSecond = tm.Second;
+		
+		subSecond = second();
+		sensors.requestTemperatures();
+		temperature = sensors.getTempCByIndex(0);
 		clearLCD();
 		lcd.setCursor(0, 0);
 		displayTime();
@@ -46,22 +51,17 @@ void loop() {
 
 void displayTime(){
 	clearBuff();
-	sprintf(buff, "%02d/%02d %02d:%02d:%02d", tm.Month, tm.Day, tm.Hour, tm.Minute, tm.Second);
+	sprintf(buff, "%02d/%02d/%02d", tmYearToCalendar(tm.Year), tm.Month, tm.Day);
 	lcd.print(buff);
 }
 
 void displayTemp(){
-	temperature = dht.getTemperature();
-	humidity = dht.getHumidity();
-
-	byte iTemp, decTemp, iHum, decHum;
+	byte iTemp, decTemp;
 	iTemp = (byte)temperature;
 	decTemp = (temperature - iTemp) * 100;
-	iHum = (byte)humidity;
-	decHum = (humidity - iHum) * 100;
 
 	clearBuff();
-	sprintf(buff, "%02d.%02dC %02d.%02d%%", iTemp, decTemp, iHum, decHum);
+	sprintf(buff, "%02d:%02d:%02d %02d.%02dC", tm.Hour, tm.Minute, tm.Second, iTemp, decTemp);
 	lcd.print(buff);
 }
 
